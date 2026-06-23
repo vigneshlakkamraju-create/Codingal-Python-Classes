6edacf0f2339f35650b86cf6b2706c9a0d9192b6
vigneshlakkamraju-create <vigneshlakkamraju@gmail.com>
2026-06-23 13:08:58 -0400

    Initial commit

---
+++ Activity1Lesson1.py
@@ -0,0 +1,8 @@
+#Printing a greeting message
+print("Hello World!")
+#Dispaly Name
+print("My name is vignesh")
+#Printing a fact
+print("I love playing soccer")
+#Performing and printing simple calculation
+print("The result of 3 + 5 is: ", 3+5)
